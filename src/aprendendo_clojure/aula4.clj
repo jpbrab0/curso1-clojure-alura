8fba39191f4973608752fc7eaed84e7d81ad52a7
Joao Pedro <jpresende502@gmail.com>
2021-05-12 21:19:50 -0300

    Terminando mais uma aula

--- src/aprendendo_clojure/aula4.clj
+++ src/aprendendo_clojure/aula4.clj
@@ -6,3 +6,30 @@
 
 (println (assoc estoque :cadeira 3))
 (println (assoc estoque :mochila 2))
+
+(println (dissoc estoque :mochila 2))
+
+(def pedido {
+             :mochila { :quantidade 2, :preco 80}
+             :camiseta { :quantidade 3, :preco 40 }})
+
+(println "\n"pedido)
+
+(def pedido
+  (assoc pedido :chaveiro {:quantidade 1, :preco 10}))
+(println pedido)
+(println (pedido :mochila))                                 ; Mapa como função
+(println (get pedido :mochila))
+(println (:mochila pedido))
+
+(println (:quantidade (:mochila pedido)))
+
+(println (update-in pedido [:mochila :quantidade] inc))
+
+; Threading
+;; Em outras linguagens "pedido.get(mochila).get(quantidade)"
+(-> pedido
+    :mochila
+    :quantidade
+    println)
+
